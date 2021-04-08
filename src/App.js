0eefd494d318ad64dbda68d716acac7dc420435b
smileyqp <468417415@qq.com>
2021-04-08 15:19:53 +0800

    polish

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import { Map, Marker } from 'react-amap';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+          <Map  amapkey='55c394d141400f8ba76c225604324470' zoom='15' center={{longitude:113.4, latitude: 33.8 }}>
+          <Marker 
+            position={{longitude:113.4, latitude: 33.8 }}
+            label = {
+              {content:   '账单',
+              position: 'BM',
+              minZoom: 15}
+            }
+            // marker={{offset: [-170, -70],minZoom: 15}}
+          ></Marker>
+           <Marker 
+            position={{longitude:113.093, latitude: 33.01212 }}
+            label = {{content: '地点'}}
+            // marker={{offset: [-170, -70],minZoom: 15}}
+          ></Marker>
+          </Map>
     </div>
   );
 }
